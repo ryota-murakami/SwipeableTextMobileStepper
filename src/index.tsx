f4d9b510951f3db42ccabde898deeffbf1dcf316
ryota-murakami <dojce1048@gmail.com>
2023-02-26 21:23:38 +0900

    fix broken project construction

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
 import Demo from './demo';
 
-ReactDOM.createRoot(document.querySelector("#root")).render(
+createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <Demo />
